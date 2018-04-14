8123b36394d4258612c95b29efb55b8a3157c191
Łukasz Durniat <ldurniat@gmail.com>
2018-04-14 19:09:16 +0200

    Fix: Use pixels per frames

--- deltatime.lua
+++ deltatime.lua
@@ -3,23 +3,31 @@ local lastTime = 0
 
 local M = {}
 
-function M.getTime() 
+function M.getTime()
+
 	local dt = 0
 
 	if lastTime == 0 then
+
 		lastTime = getTimer()
+
 	else	
+
 		local curTime = getTimer()
 		dt = curTime - lastTime
-		dt = dt / ( 1000 / display.fps )
-		lastTime = curTime			
+		dt = dt / 1000 
+		lastTime = curTime	
+
 	end	
 
 	return dt
+
 end	
 
 function M.restart()
+
 	lastTime = 0
+
 end	
 
 return M
